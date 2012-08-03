358dbaf15fc022aecdb8bbabde3101accab6cca9
Matthew Barbour <sanmadjack@gmail.com>
2012-08-03 08:44:33 -0500

    Initial commit

---
+++ enums/RegRoot.cs
@@ -0,0 +1,12 @@
+﻿
+namespace GameSaveInfo {
+    public enum RegRoot {
+        classes_root,
+        current_user,
+        current_config,
+        dyn_data,
+        local_machine,
+        performace_data,
+        users
+    }
+}
